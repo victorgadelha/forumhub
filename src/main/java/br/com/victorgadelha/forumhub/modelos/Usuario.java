9af80ef62e815806e7a7e8b0472d865ebd89d197
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-07-16 15:57:04 -0300

    feat: criando entidade de usuario

---
+++ src/main/java/br/com/victorgadelha/forumhub/modelos/Usuario.java
@@ -0,0 +1,44 @@
+package br.com.victorgadelha.forumhub.modelos;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Email(message = "O email deve ser válido.")
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Column(unique = true)
+    private String senha;
+
+}
